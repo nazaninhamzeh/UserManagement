9171d13f4a7f844e128cd7867881cbe15396ce4b
Nazanin Hamzeh <your_email@example.com>
2025-12-03 21:09:49 +0330

    feat: add user entity

---
+++ src/main/java/com/souldev/authservice/entity/User.java
@@ -0,0 +1,30 @@
+package com.souldev.authservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table( name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @EqualsAndHashCode.Include
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String firstName;
+    private String lastName;
+
+}
