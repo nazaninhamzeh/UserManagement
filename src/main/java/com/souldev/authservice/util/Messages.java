c8a0e48f46ea2fa229367934c20bb787affcf72b
Nazanin Hamzeh <your_email@example.com>
2025-12-03 21:13:10 +0330

    feat: add Messages to handle Exception messages

---
+++ src/main/java/com/souldev/authservice/util/Messages.java
@@ -0,0 +1,8 @@
+package com.souldev.authservice.util;
+
+public class Messages {
+
+    public static final String USER_ALREADY_EXISTS = "User with this username already exists";
+    public static final String USER_NOT_FOUND_BY_ID = "User with Id %s not found";
+    public static final String USER_NOT_FOUND_BY_USERNAME = "User with Username %s not found";
+}
