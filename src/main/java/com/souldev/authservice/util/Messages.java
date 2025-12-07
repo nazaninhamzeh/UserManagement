3db8451b247682fab9b833e50dc498f85008bcb6
Nazanin Hamzeh <your_email@example.com>
2025-12-07 16:56:18 +0330

    feat: adding UserNotFound message

--- src/main/java/com/souldev/authservice/util/Messages.java
+++ src/main/java/com/souldev/authservice/util/Messages.java
@@ -5,4 +5,5 @@ public class Messages {
     public static final String USER_ALREADY_EXISTS = "User with this username already exists";
     public static final String USER_NOT_FOUND_BY_ID = "User with Id %s not found";
     public static final String USER_NOT_FOUND_BY_USERNAME = "User with Username %s not found";
+    public static final String USER_NOT_FOUND =  "User not found";
 }
